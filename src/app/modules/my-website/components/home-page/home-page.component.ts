ef95c4c1a05eab9c1a1d9e565802ce4b781b8764
12jikan <jareem.e.hoff@gmail.com>
2019-01-30 12:26:01 -0500

    this is the first update in a while

--- src/app/modules/my-website/components/home-page/home-page.component.ts
+++ src/app/modules/my-website/components/home-page/home-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
+
 export class HomePageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
+    let typed = new Typed('#typed', {
+      strings: ["I am ^1000 a Designer", "I am ^1000 a Developer", "I am ^1000 a Programmer", "I am ^1000 a Creator" ],
+      smartBackspace: true,
+      loop: true,
+      cursorChar: '|',
+      typeSpeed: 50,
+      backSpeed: 80,
+
+    });
+
   }
 
 }
